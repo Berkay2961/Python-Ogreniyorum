a26b26dafa1e08bd8a11ebfaf2262a75132499a1
Berkay2961 <156474257+Berkay2961@users.noreply.github.com>
2025-07-21 19:13:12 +0300

    Add files via upload

---
+++ Python Baslangic.py
@@ -0,0 +1,39 @@
+#ekrana yazdirma
+print("Merhaba Python")
+print("Ad:Berkay")
+print("Soyad:Sahin")
+print("Yas:20")
+print("Meslek:Ogrenci")
+#ekrana yazdirma diger yol
+print("Ad:Berkay,Soyad:Sahin,Yas:20,Meslek:Ogrenci")
+
+#degisken tanimlama ve ekrana yazdirma
+yas=20
+boy=1.85
+kilogram=90
+print("Yas",yas)
+print("Boy",boy)
+print("Kilogram",kilogram)
+
+#iki sayi tanimla ve dort islem metni yazdir
+sayi1= 18
+sayi2 = 22
+
+toplam=sayi1+sayi2
+fark=sayi1-sayi2
+carpim=sayi1*sayi2
+bolum=sayi1/sayi2
+
+print("Toplam",toplam)
+print("Fark",fark)
+print("Carpim",carpim)
+print("Bolum",bolum)
+
+#kullanicidan yas bilgisini alip 5 yil sonraki yasini yazdirma
+yas= int(input("Yasinizi girin:"))
+gelecekYas= yas+5
+print("5 yil sonraki yasiniz:", gelecekYas)
+
+
+
+
