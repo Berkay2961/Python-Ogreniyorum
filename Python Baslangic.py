1a9f8c7b6ba2888b76d78e1f1f71a0cbf6f1d970
Berkay2961 <156474257+Berkay2961@users.noreply.github.com>
2025-07-23 19:40:01 +0300

    Ekleme yapıldı
    for döngüsü bitirildi

--- Python Baslangic.py
+++ Python Baslangic.py
@@ -122,6 +122,28 @@ for i in range(5):
 ortalama = sum(sayi_listesi) / len(sayi_listesi)
 print("Girilen sayilarin ortalamasi:", ortalama)
 
+#1 den 10 a kadar çarpım tablosunu yazdır
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i * j}")
+    
+    # 100 e kadar olan sayıların toplamını hesaplama
+toplam = 0
+for i in range(1, 101):
+    toplam += i 
+print(" 100'e kadar olan sayıların toplamı:", toplam)
+
+# kullanıcının girdiği sayıya kadar olan çift sayıları listeleme
+sayi = int(input("Bir sayi girin: "))
+cift_sayilar = []
+for i in range(2, sayi + 1, 2):
+    cift_sayilar.append(i)
+print("Girilen sayıya kadar olan çift sayılar:", cift_sayilar)
+
+
+
+
+
 
 
 
